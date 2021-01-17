617e4034b9b82bdc23e87c51158facb53c69c5ca
CSWILIK <cswilik@gmail.com>
2021-01-17 13:24:35 -0500

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
@@ -6,6 +6,53 @@ import PetBrowser from "./PetBrowser";
 function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
+
+  useEffect(() => {
+    fetch("http://localhost:3001/pets")
+    .then(r => r.json())
+    .then(data => setPets(data))
+  }, [])
+
+  function handleFilter(selectedFilter) {
+    setFilters({type: selectedFilter})
+    
+  }
+
+  function handleFindPetsClick() {
+    let url = "http://localhost:3001/pets";
+
+    if (filters.type !== "all") {
+      url += `?type=${filters.type}`;
+    }
+
+    fetch(url)
+      .then((r) => r.json())
+      .then((petsArray) => {
+        setPets(petsArray);
+      });
+  }
+
+  function handleAdoptPet(id) {
+    // grab pet.id from Pet Component
+    // map over pets to find pet with corresponding id
+    //  update pet.isAdopted==true
+    // add this pet to the pets array
+    const updatedPets = pets.map(pet => {
+      console.log(pet)
+      if (pet.id === id) {
+        pet.isAdopted = true
+        return pet
+      } else return pet
+    })
+    //  update pet.isAdopted==true
+    // add this pet to the pets array
+    console.log(updatedPets)
+    setPets(updatedPets) 
+    
+
+  }
+
+
 
   return (
     <div className="ui container">
@@ -15,10 +62,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType={handleFilter} onFindPetsClick = {handleFindPetsClick}/>
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets = {pets} onAdoptPet={handleAdoptPet}/>
           </div>
         </div>
       </div>
